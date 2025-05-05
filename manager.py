b79cadbc7978ab4fa81a726fd02192c7e78a32c3
Sharatchandra <sharatchandrats@gmail.com>
2025-05-05 23:21:25 +0530

    Added delete functaionality
    Added id for passwords saved, making it easier to delete(which has been added) and update functaionalities

--- manager.py
+++ manager.py
@@ -1,4 +1,6 @@
 import csv
+import copy
+
 from models.model_pass import password
 import models.model_pass as model_pass
 
@@ -11,28 +13,52 @@ fields = []
 # Stores the data
 all_passwords = []
 
+# Stores all data
+all_data = []
+
 # Loads all the passwords from the csv file
 with open(file_name, 'r') as file:
-        reader = csv.reader(file)
+    reader = csv.reader(file)
 
-        # Returns the first row which in this case is the field names
-        fields = next(reader)
- 
-        # Adds all the passwords to the list
-        for row in reader:
-            # Invokes menthod from model_pass for ease
-            all_passwords.append(model_pass.convert_list_to_pwd(row))
+    # Stores all data
+    all_data = list(reader)
+
+    # Returns the first row which in this case is the field names
+    fields = all_data[0]
+
+    # Adds all the passwords to the list
+    for row in all_data[1:]:
+        # Invokes menthod from model_pass for ease
+        all_passwords.append(model_pass.convert_list_to_pwd(row))
 
 
 # Returns the list of all passwords stored
 def get_all_passwords():
     return all_passwords
 
+# Function to add a new password to the database
 def add_password(pwd):
-     with open(file_name, 'a', newline='') as file:
-          writer = csv.writer(file)
-          
-          # Appends a new row in the csv with the new password
-          writer.writerow(model_pass.convert_pwd_to_list(pwd))
-          
+    with open(file_name, 'a', newline='') as file:
+        writer = csv.writer(file)
+        pwd.id = len(all_passwords)
+        # Appends a new row in the csv with the new password
+        writer.writerow(model_pass.convert_pwd_to_list(pwd))
 
+# Function to delete a password
+def delete_password(id):
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+
+        # Ensures a deep copu of all data list so, when data is modified, all data dosent change
+        data = copy.deepcopy(all_data)
+        
+        # Trys to delete the item. If failure, throws an exception
+        try:
+            data.pop(id)
+        except IndexError:
+            print(IndexError)
+        else:
+            for i in range(id, len(data)):
+                data[i][0] = str(int(data[i][0]) - 1)
+        
+        writer.writerows(data)
--- models/model_pass.py
+++ models/model_pass.py
@@ -1,11 +1,12 @@
 class password:
-    def __init__(self, org, username, password):
+    def __init__(self, id, org, username, password):
+        self.id = id
         self.org = org
         self.username = username
         self.password = password
 
 def convert_list_to_pwd(list):
-    return password(org=list[0], username=list[1], password=list[2])
+    return password(org=list[1], username=list[2], password=list[3], id= list[0])
 
 def convert_pwd_to_list(pwd):
-    return [pwd.org, pwd.username, pwd.password]
+    return [pwd.id, pwd.org, pwd.username, pwd.password]
