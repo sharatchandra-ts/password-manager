7d1d1c94a01f01860e63454dad7819cacb43dde9
Sharatchandra <sharatchandrats@gmail.com>
2025-05-13 16:59:23 +0530

    Stable commit
    Added delete feature, and working disabled buttons

--- gui.py
+++ gui.py
@@ -11,7 +11,19 @@ website_var = tkinter.StringVar()
 password_var = tkinter.StringVar()
 
 passwordManager = PasswordManager()
-all_passwords = passwordManager.get_all_passwords()
+all_passwords = passwordManager.load_data()
+
+def refresh_tree():
+    # Clear current table rows
+    for item in tree.get_children():
+        tree.delete(item)
+
+    # Reload passwords
+    updated_passwords = passwordManager.load_data()
+
+    # Reinsert rows
+    for password in updated_passwords:
+        tree.insert("", "end", values=(password.id, password.org, password.username, password.password))
 
 # Handels the show password function
 def checkboxCommand():
@@ -21,21 +33,58 @@ def checkboxCommand():
         pwd_entry.config(show='*')
 
 # Handels the action to be taken when the button is clicked
-def btnCommand():
-    print(username_var.get(), website_var.get(), password_var.get())
+def funBtnAdd():
     pwd = Password(id=None, org=website_var.get(), username=username_var.get(), password=password_var.get())
     passwordManager.add_password(pwd=pwd)
+    refresh_tree()
+
+def funBtnDelete():
+    selected_items = tree.selection()
+    ids_to_delete = []
+
+    for item in selected_items:
+        item_values = tree.item(item, "values")
+        ids_to_delete.append(int(item_values[0]))
+
+    for id in sorted(ids_to_delete, reverse=True):
+        passwordManager.delete_password(id)
+
+    refresh_tree()
+
+def funBtnUpdate():
+    pass
+
+def onSelect(event):
+    selected_items = event.widget.selection()
+    if len(selected_items) == 1:
+        btn_delete.state(['!disabled'])
+        btn_update.state(['!disabled'])
+    elif len(selected_items) >= 1:
+        btn_update.state(['disabled'])
+        btn_delete.state(['!disabled'])
+    else:
+        btn_delete.state(['disabled'])
+        btn_update.state(['disabled'])
+
+def check_inputs(*_):
+    if website_var.get().strip() != "" and username_var.get().strip() != "" and len(password_var.get().strip()) >= 8:
+        btn_add.state(["!disabled"])
+    else:
+        btn_add.state(["disabled"])
 
 
 frame_entry = ttk.Frame(root)
 frame_entry.grid(pady=50, padx=25, column=0, row=0)
 
 frame_display = ttk.Frame(root)
-frame_display.grid(pady=50, padx=25, column=1, row=0)
+frame_display.grid(pady=15, padx=25, column=1, row=0)
+
+frame_edit = ttk.Frame(root)
+frame_edit.grid(pady=15, padx=0, column=1, row=1)
 
 root.title("My App")
 
-ttk.Label(frame_entry, text="ADD NEW PASSWORD\n").grid()
+# ttk.Label(frame_entry, text="ADD NEW PASSWORD\n").grid()
 
 # Text field for website name entry
 ttk.Label(frame_entry, text="Enter website name: ").grid(row=1, column=0)
@@ -53,7 +102,13 @@ pwd_entry.grid(row=3,column=1)
 
 ttk.Checkbutton(frame_entry, text="Show password", variable=checkbox_var, command= checkboxCommand).grid(row=4,column=1)
 
-ttk.Button(frame_entry, text="Submit", command=btnCommand).grid(row=5,column=1)
+btn_add = ttk.Button(frame_entry, text='Add Entry', command=funBtnAdd)
+btn_add.grid(row=5,column=1)
+btn_add.state(['disabled'])
+
+website_var.trace_add('write', check_inputs)
+username_var.trace_add('write', check_inputs)
+password_var.trace_add('write', check_inputs)
 
 # This is the table to show the list of passwords
 tree = ttk.Treeview(frame_display, columns=('i','o','u','p'), show="headings")
@@ -67,9 +122,19 @@ tree.column("o",  width=100)
 tree.column("u", width=100)
 tree.column("p", width=100)
 
+tree.bind("<<TreeviewSelect>>", onSelect)  # Bind select event
+
 # Adds evey password into the table
 for password in all_passwords:
     tree.insert("", "end", values=(password.id, password.org, password.username, password.password))
-tree.grid(row=1, column=10)
+tree.grid(row=0, column=0)
+
+btn_delete = ttk.Button(frame_edit, text='Delete', command=funBtnDelete)
+btn_delete.grid(row=0, column=0)
+btn_delete.state(['disabled'])
+
+btn_update = ttk.Button(frame_edit, text='Update', command=funBtnUpdate)
+btn_update.grid(row=0, column=1)
+btn_update.state(['disabled'])
 
 root.mainloop()
--- manager.py
+++ manager.py
@@ -5,10 +5,17 @@ from models.model_pass import Password
 import models.model_pass as model_pass
 
 class PasswordManager:
-
+    
     def __init__(self):    
         # Name/Location of passwords stored file csv
         self.file_name = "database/passwords.csv"
+
+        # Loads all the passwords from the csv file
+        self.load_data()
+
+
+    # Returns the list of all passwords stored
+    def load_data(self):
 
         # Stores the field names
         self.fields = []
@@ -21,26 +28,21 @@ class PasswordManager:
 
         self.length = 0
 
-        # Loads all the passwords from the csv file
         with open(self.file_name, 'r') as file:
             reader = csv.reader(file)
-
             # Stores all data
-            all_data = list(reader)
+            self.all_data = list(reader)
 
             # Returns the first row which in this case is the field names
-            fields = all_data[0]
+            self.fields = self.all_data[0]
 
             # Adds all the passwords to the list
-            for row in all_data[1:]:
+            for row in self.all_data[1:]:
                 # Invokes menthod from model_pass for ease
                 self.all_passwords.append(model_pass.convert_list_to_pwd(row))
 
             self.length = len(self.all_passwords)
 
-
-    # Returns the list of all passwords stored
-    def get_all_passwords(self):
         return self.all_passwords
 
     # Function to add a new password to the database
@@ -50,7 +52,6 @@ class PasswordManager:
             pwd.id = self.length
             # Appends a new row in the csv with the new password
             writer.writerow(model_pass.convert_pwd_to_list(pwd))
-            self.length += 1
 
     # Function to update password
     def update_password(self, pwd):
@@ -70,19 +71,20 @@ class PasswordManager:
 
     # Function to delete a password
     def delete_password(self, id):
+        id += 1  # Adjust for header row
+        with open(self.file_name, 'r', newline='') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+
+        # Trys to delete the item. If failure, throws an exception
+        try:
+            data.pop(id)
+        except IndexError:
+            print("IndexError: ID not found")
+        else:
+            for i in range(id, len(data)):
+                data[i][0] = str(int(data[i][0]) - 1)
+
         with open(self.file_name, 'w', newline='') as file:
             writer = csv.writer(file)
-
-            # Ensures a deep copu of all data list so, when data is modified, all data dosent change
-            data = copy.deepcopy(self.all_data)
-            
-            # Trys to delete the item. If failure, throws an exception
-            try:
-                data.pop(id)
-            except IndexError:
-                print(IndexError)
-            else:
-                for i in range(id, len(data)):
-                    data[i][0] = str(int(data[i][0]) - 1)
-            
             writer.writerows(data)
